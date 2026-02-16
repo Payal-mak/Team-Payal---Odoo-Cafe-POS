30992bcc46b8d63f9333fec87a0f10855aa11b47
Payal-mak <iampayal018@gmail.com>
2026-02-16 19:29:25 +0530

    feat: added necessary sql files

---
+++ database/add_product_images.sql
@@ -0,0 +1,98 @@
+-- Update products with image URLs
+-- You can replace these with actual image URLs from your image hosting service
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=400'
+WHERE
+    name = 'Cappuccino';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?w=400'
+WHERE
+    name = 'Latte';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1484244233201-29892afe6a2c?w=400'
+WHERE
+    name = 'Americano';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1578374173703-2c7e2b7a3d7f?w=400'
+WHERE
+    name = 'Mocha';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1571934811356-5cc061b6821f?w=400'
+WHERE
+    name = 'Masala Chai';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=400'
+WHERE
+    name = 'Green Tea';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1597318130159-b84a0d6d6e5f?w=400'
+WHERE
+    name = 'Lemon Tea';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1600271886742-f049cd451bba?w=400'
+WHERE
+    name = 'Fresh Juice';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1505252585461-04db1eb84625?w=400'
+WHERE
+    name = 'Smoothie';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?w=400'
+WHERE
+    name = 'Cold Coffee';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?w=400'
+WHERE
+    name = 'Sandwich';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400'
+WHERE
+    name = 'Burger';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1573080496219-bb080dd4f877?w=400'
+WHERE
+    name = 'French Fries';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=400'
+WHERE
+    name = 'Chocolate Cake';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1607920591413-4ec007e70023?w=400'
+WHERE
+    name = 'Brownie';
+
+UPDATE products
+SET
+    image_url = 'https://images.unsplash.com/photo-1563805042-7684c019e1cb?w=400'
+WHERE
+    name = 'Ice Cream';
